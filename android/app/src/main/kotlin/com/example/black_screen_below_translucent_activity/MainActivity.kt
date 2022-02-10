2402a5d6eee3214a1b22b047afcfa3758ea2e8ef
Dot Cink <Dot.Cink+github@gmail.com>
2022-02-10 23:02:51 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/black_screen_below_translucent_activity/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.black_screen_below_translucent_activity
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
